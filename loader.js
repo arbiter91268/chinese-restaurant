390376bbe78f4271aa9926b32326e87976d69359
Kristian Hansen <kris91268@gmail.com>
2015-12-07 11:19:38 +1300

    Designs + More Code
    More code has been written, and some designs made.
    Should I use XML or JSON?

---
+++ loader.js
@@ -0,0 +1,10 @@
+// constants 
+var ORDER_CHEAP_ITEMS_FIRST = true;
+var IGNORE_ITEM_GROUPS = false;
+
+$(document).read(function() {
+    $.getJSON("data/lunch_menu.json", function(data) {
+        var dishes = [];
+        
+    });
+});
