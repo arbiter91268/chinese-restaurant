39d466158133e1bfc93e52d028caa4a28c36013e
Kristian Hansen <kris91268@gmail.com>
2015-12-11 16:05:50 +1300

    Seafood Section
    Completed the seafood section of the menu, plus a few other things.

--- loader.js
+++ loader.js
@@ -1,10 +1,47 @@
 // constants 
 var ORDER_CHEAP_ITEMS_FIRST = true;
 var IGNORE_ITEM_GROUPS = false;
+var SEASONAL_PRICE_STRING = "Seasonal Price";
 
-$(document).read(function() {
+$(document).ready(function() {
+    var test = [54, 34, 75, 23, 1, 86, 34, 24];
+    console.log(test.sort(function(a, b) {return a - b;}));
+    console.log(orderPrices([54, 34, 75, 23, 1, 86, 34, 24])); // a simple test
     $.getJSON("data/lunch_menu.json", function(data) {
         var dishes = [];
-        
+        $.each(data, function(i, field) {
+            $.each(field, function(i, field) {
+                dishes.push(field);
+                if ($.get(data, "price") == -1) {
+                    data = SEASONAL_PRICE_STRING;
+                }
+            });
+        }); 
+        console.log(dishes);
     });
 });
+
+function orderPrices(data) {
+    var ordered = [];
+    var count = 0;
+    var indicesToIgnore = [];
+    while (count < data.length) {
+        var smallest = 10000;
+        var index = 0;
+        checker: for (var i = 0; i < data.length; i++) {
+            for (var j in indicesToIgnore) {
+                if (i == j) {
+                    continue checker;
+                }
+            }
+            if (data[i] <= smallest) {
+                smallest = data[i];
+                index = i;
+            }
+        }
+        ordered.push(smallest);
+        indicesToIgnore.push(index);
+        count++;
+    }
+    return ordered;
+}
